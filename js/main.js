74e4e729ced5f870b5743e689a603295733318bb
mikesmithjr <mikesmithjr@fullsail.edu>
2012-10-04 02:17:17 -0500

    Device Info working now working on battery info

--- js/main.js
+++ js/main.js
@@ -21,11 +21,7 @@ AVF Test Page
 		var location = latitude+","+longitude
 		var mapImg="http://maps.googleapis.com/maps/api/staticmap?center="+location+"&markers=color:blue%7C"+location+"&zoom=14&size=400x300&sensor=false";
 		element.innerHTML = "<img src='"+mapImg+"'>";
-							//"Latitude: " +position.coords.latitude+ "<br />"+
-							//"Longitude: " +position.coords.longitude+ "<br />"
-							
-							//"<img src= 'http://maps.googleapis.com/maps/api/staticmap?zoom=15&amp;size=480x280&amp;maptype=roadmap&amp;markers=color:red|'"+latitude+"','"+longitude+"'&amp;sensor=true'>"
-							//'<img src="http://maps.google.com/maps/api/staticmap?zoom=15&size=540x280&maptype=roadmap&markers=color:red|33.2845474,-96.7817025&sensor=true">'
+							 
 	};
 
 	// onError Callback receives a PositionError object
@@ -33,7 +29,7 @@ AVF Test Page
 	        alert('code: '    + error.code    + '\n' +
 	              'message: ' + error.message + '\n');
 	    };
-
+	//On Screen Alert
 	document.addEventListener("deviceready", deviceReady, false);
 
 	function deviceReady(){
@@ -48,3 +44,34 @@ AVF Test Page
 
 
 	};
+	//Display Device Info
+	document.addEventListener("deviceready", devInfo, false);
+
+	function devInfo(){
+		var element = document.getElementById('devInfo');
+
+        element.innerHTML = '<li>Device Name: '     + device.name     + '</li>' + 
+                            '<li>Device Cordova: '  + device.cordova + '</li>' + 
+                            '<li>Device Platform: ' + device.platform + '</li>' +  
+                            '<li>Device Version: '  + device.version  + '</li>';
+
+	};
+
+	//Display Battery Status
+	function pageLoad(){
+		document.addEventListener("deviceready", battInfo, false);
+	};
+
+	function battInfo(){
+		window.addEventListener("batterystatus", battStats, false);
+	};
+	
+	function battStats(info){
+		var element = document.getElementById('battInfo');
+		element.innerHTML = '<li>Battery Level: ' + info.level + '</li>' +  
+                        	'<li>Charging Status: '  + info.isPlugged  + '</li>';
+
+	};
+
+
+	
