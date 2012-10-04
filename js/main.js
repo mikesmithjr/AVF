91f7c1a87e06ce9e93eaf363c179771f62d0930d
mikesmithjr <mikesmithjr@fullsail.edu>
2012-10-04 01:08:52 -0500

    Maps and notification working on both devices

--- js/main.js
+++ js/main.js
@@ -18,10 +18,14 @@ AVF Test Page
 		var element = document.getElementById("map");
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
-		element.innerHTML = //"Latitude: " +position.coords.latitude+ "<br />"+
+		var location = latitude+","+longitude
+		var mapImg="http://maps.googleapis.com/maps/api/staticmap?center="+location+"&markers=color:blue%7C"+location+"&zoom=14&size=400x300&sensor=false";
+		element.innerHTML = "<img src='"+mapImg+"'>";
+							//"Latitude: " +position.coords.latitude+ "<br />"+
 							//"Longitude: " +position.coords.longitude+ "<br />"
-							//"<img src= 'http://maps.google.com/maps/api/staticmap?zoom=5&amp;size=480x280&amp;maptype=roadmap&amp;markers=color:red'"+latitude+"','"+longitude+"'&amp;sensor=true'>"
-							'<img src="http://maps.google.com/maps/api/staticmap?zoom=5&size=540x280&maptype=roadmap&markers=color:red|33.2845474,-96.7817025&sensor=true">'
+							
+							//"<img src= 'http://maps.googleapis.com/maps/api/staticmap?zoom=15&amp;size=480x280&amp;maptype=roadmap&amp;markers=color:red|'"+latitude+"','"+longitude+"'&amp;sensor=true'>"
+							//'<img src="http://maps.google.com/maps/api/staticmap?zoom=15&size=540x280&maptype=roadmap&markers=color:red|33.2845474,-96.7817025&sensor=true">'
 	};
 
 	// onError Callback receives a PositionError object
@@ -30,9 +34,17 @@ AVF Test Page
 	              'message: ' + error.message + '\n');
 	    };
 
-	document.addEventListener("deviceready", myAlert, false);
+	document.addEventListener("deviceready", deviceReady, false);
+
+	function deviceReady(){
+		//nothing
+	};
+	function dismissed(){
+		//not sure
+	};
+
 	function myAlert(){
-		navigator.notification.alert('You are kinda hot!', 'Hey You!', 'I know');
+		navigator.notification.alert('You are kinda hot!', dismissed, 'Hey You!', 'I know');
 
 
 	};
