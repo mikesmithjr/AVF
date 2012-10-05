b59808acec9c3ec1ba3159ce2aae29f781e68b1c
mikesmithjr <mikesmithjr@fullsail.edu>
2012-10-05 01:45:31 -0500

    Cannot get any other function to work right

--- js/main.js
+++ js/main.js
@@ -4,8 +4,58 @@ Michael Smith Jr.
 AVF Test Page
 */
 
+	//Accelerometer Test Page
 
-	//Waiting for PhoneGap to load
+	 // The watch id references the current `watchAcceleration`
+    var watchID = null;
+
+    // Wait for Cordova to load
+    //
+    document.addEventListener("deviceready", accelTest, false);
+
+    // Cordova is ready
+    //
+    function accelTest() {
+        startWatch();
+    };
+
+    // Start watching the acceleration
+    //
+    function startWatch() {
+
+        // Update acceleration every 3 seconds
+        var options = { frequency: 3000 };
+
+        watchID = navigator.accelerometer.watchAcceleration(onSuccess, onError, options);
+    };
+
+    // Stop watching the acceleration
+    //
+    function stopWatch() {
+        if (watchID) {
+            navigator.accelerometer.clearWatch(watchID);
+            watchID = null;
+        };
+    };
+
+    // onSuccess: Get a snapshot of the current acceleration
+    //
+    function onSuccess(acceleration) {
+        var element = document.getElementById('accelerometer');
+        element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+                            'Acceleration Y: ' + acceleration.y + '<br />' +
+                            'Acceleration Z: ' + acceleration.z + '<br />' +
+                            'Timestamp: '      + acceleration.timestamp + '<br />';
+    };
+
+    // onError: Failed to get the acceleration
+    //
+    function onError() {
+        alert('onError!');
+    };
+
+    
+	//Map Test Page
 	document.addEventListener("deviceready", onMapLoad, false);
 
 	//Pulling Info from GPS
@@ -18,7 +68,7 @@ AVF Test Page
 		var element = document.getElementById("map");
 		var latitude = position.coords.latitude;
 		var longitude = position.coords.longitude;
-		var location = latitude+","+longitude
+		var location = latitude+","+longitude;
 		var mapImg="http://maps.googleapis.com/maps/api/staticmap?center="+location+"&markers=color:blue%7C"+location+"&zoom=14&size=400x300&sensor=false";
 		element.innerHTML = "<img src='"+mapImg+"'>";
 							 
@@ -51,27 +101,16 @@ AVF Test Page
 		var element = document.getElementById('devInfo');
 
         element.innerHTML = '<li>Device Name: '     + device.name     + '</li>' + 
-                            '<li>Device Cordova: '  + device.cordova + '</li>' + 
-                            '<li>Device Platform: ' + device.platform + '</li>' +  
-                            '<li>Device Version: '  + device.version  + '</li>';
+                            '<li>PhoneGap Version: '  + device.cordova + '</li>' + 
+                            '<li>Device Operating System: ' + device.platform + '</li>' +  
+                            '<li>Operating System Version: '  + device.version  + '</li>';
 
 	};
 
-	//Display Battery Status
-	function pageLoad(){
-		document.addEventListener("deviceready", battInfo, false);
-	};
 
-	function battInfo(){
-		window.addEventListener("batterystatus", battStats, false);
-	};
-	
-	function battStats(info){
-		var element = document.getElementById('battInfo');
-		element.innerHTML = '<li>Battery Level: ' + info.level + '</li>' +  
-                        	'<li>Charging Status: '  + info.isPlugged  + '</li>';
 
-	};
+
+
 
 
 	
